dd7f36e46f1c96a86a2a1c8a58453854b5f569b1
zahra362 <74509514+zahra362@users.noreply.github.com>
2025-11-29 21:15:57 +0330

    Add files via upload

---
+++ chapter1_exercise3.py
@@ -0,0 +1,7 @@
+#chapter 4 - QUICK CHECK: PYTHONIC STYLE
+
+print("'bar()' and 'foo_bar()' are good Pythonic function-naming styles: all lowercase with underscores for readability.")
+print("'really_very_long_var_name' and 'foobar' are good Pythonic variable-naming styles: all lowercase with underscores for readablitiy")
+print("'longvarname' and 'foobar' are not good Pythonic variable-naming styles: they lack underscores for readablitiy")
+print("'VERYLONGVARNAME' is not a good Pythonic variable-naming style: all letters are uppercase and it lacks underscores for readability. ")
+print("'varName' is not a good Pythonic variable-naming style: it uses camelCase instead of lowercase_with_underscores.")
